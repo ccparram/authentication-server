719ec570c0f03af10fd4d106db3ad409c0a083de
Camilo Parra <ccparram@unal.edu.co>
2016-05-18 23:36:58 -0500

    Added limit to body-parse to 100 mb

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
+app.use(bodyParser({limit: '100mb'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
